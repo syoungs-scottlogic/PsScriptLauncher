1271f9f2bac141043edec43cb288db5a1db94102
syoungs <syoungs@scottlogic.com>
2022-10-07 10:54:00 +0100

    Added logic for agentScript

--- agent/agentScript.sh
+++ agent/agentScript.sh
@@ -1,4 +1,8 @@
+#!/usr/bin/bash
+
 # Get Instance ID on machine. 
-# ec2-metadata i- 
+INSTANCEID=$(ec2-metadata -i | grep -E -o '(i-)\w+')
 # instance-id: i-0da6eed6ab8a045a2
 
+BUCKET="sy-scriptlaunchtest"
+aws s3 cp s3://${BUCKET}/${INSTANCEID}/ ./ --recursive
